d0acbac9904e5afcfeae8624e6872ecbe7575221
huiru-wang <huiru-wang@outlook.com>
2025-01-19 03:13:25 +0800

    basePath

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,15 +4,14 @@
 cd /root/blog
 
 # 拉取最新的 Git 代码
-echo "Pulling latest Git code..."
+echo "===================== Pulling latest Git code ====================="
 git pull
 
 # 使用 pnpm 进行重新构建
-echo "Rebuilding with pnpm..."
+echo "===================== Rebuilding with pnpm ======================="
 pnpm install
-
 pnpm build
 
 # 重启 pm2 中的应用程序，假设你的应用名称是 myapp，根据实际情况修改
-echo "Restarting pm2 application..."
+echo "===================== Restarting application ====================="
 pm2 restart blog
