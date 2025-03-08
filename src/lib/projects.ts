c6e6a15186c54f353d884810406924418a36e430
huiru-wang <huiru-wang@outlook.com>
2025-03-08 14:43:32 +0800

    update

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,20 +1,27 @@
 export const projects = [
     {
-        "redirect": "/blogs/2024-11-12.md",
+        "redirect": "/blogs/05-HR-Saas人事档案模块重构.md",
         "img": "/images/hr-saas-roster-architecture.png",
         "title": "HR Saas 人事平台花名册档案系统设计与实现",
         "description": "由于历史原因和前期的产品快速迭代，人事业务中最核心的人事档案模块，代码开始变得越来越腐化，逐渐暴露出各种问题：\n 1. 新人使用、接手、迭代的成本越来越高；\n 2. 代码架构不合理，难以支撑更复杂的产品逻辑。 这是这个人事档案重构项目开启的一个背景；",
         "publishedAt": "2024-11-12"
     },
     {
-        "redirect": "/blogs/2024-06-10.md",
+        "redirect": "/blogs/04-HR-Saas电子签平台.md",
         "img": "/images/hr-saas-esign-architecture-design.png",
         "title": "HR Saas 电子签平台构建",
         "description": "电子签署能力在人事SaaS领域的商业价值不仅体现在效率提升和成本优化上，更通过法律合规、数据智能化、生态协同及可持续发展等维度，这是一个智能化的HR Saas产品的必备能力，也是商业化、增强产品力的很好的一个功能模块。同时人事业务本身就是电子签署的最大的应用场景，人员流动、合同管理都需要高效的电子签署来支持。",
         "publishedAt": "2024-06-10"
     },
     {
-        "redirect": "/blogs/2023-12-01.md",
+        "redirect": "/blogs/03-Blog网站的构建.md",
+        "img": "https://github.com/huiru-wang/blog/blob/main/docs/black-home.png",
+        "title": "Business Check Service 资金一致性校验服务",
+        "description": "一个后端程序员尝试学习前端，制作的基于 Next.js、 TailwindCSS、next-remote-mdx 的微像素风博客。本地多目录文件加载，支持md、mdx格式文件的渲染、黑白主题、响应式布局",
+        "publishedAt": "2024-11-01"
+    },
+    {
+        "redirect": "/blogs/01-BCP资金一致性服务.md",
         "img": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons//spring/spring-original.svg",
         "title": "Business Check Service 资金一致性校验服务",
         "description": "Business Check Service服务（BCP）是专门针对交易系统的一个旁路校验服务；通过交易链路中产生的日志，进行关联核对关键信息，保证每次交易链路的资金一致性；",
