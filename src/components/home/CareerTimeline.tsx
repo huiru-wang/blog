5bbf5d324dae07a14eb6cd07d79bbe1b838d3585
huiru-wang <huiru-wang@outlook.com>
2025-03-22 13:58:45 +0800

    career time line

--- src/components/home/CareerTimeline.tsx
+++ src/components/home/CareerTimeline.tsx
@@ -7,16 +7,10 @@ const data = [
         content: (
             <div>
                 <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    这一年是我在南京的第一年，也是在长春的最后一年，我用了一年的时间，每天图书馆和宿舍2点一线，最后我如愿考上了南京理工大学的控制工程专业。9月份我只身一人来到南京，开始了我最后3年的校园生活。
+                    我用了一年的时间，每天图书馆和宿舍2点一线，最后我如愿考上了南京理工大学的控制工程专业。2018.9月份我来到南京，开始了我最后3年的校园生活，也从这一年开始初步接触计算机领域。
                 </p>
                 <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    第一次见到导师，我们聊了一些，但不多，随后我便找到师兄师姐，讨论选课的问题。我知道我来读研的目的，就是想要学到东西，找到一份好一点的工作。所以我问的第一个问题就是：“师姐，选什么课比较好找工作呀？”。
-                </p>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    师姐笑了：“那你就选最简单好过的课吧，然后自己去学计算机吧”。我愣了
-                </p>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    就这样，我的本专业还没开始，我就已经要转行👨‍💻了。
+                    刚来到学习哦，跟师兄师姐讨论选课的问题，
                 </p>
             </div>
         )
@@ -29,16 +23,10 @@ const data = [
                     这一年多时间，从计算机网络到数据结构与算法，从HelloWorld到个人网站开发，也经常和朋友在峡谷决战到天亮，每个学期末天天泡在图书馆，狂补课刷题。
                 </p>
                 <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    后来也开始在学校附近的一家云计算公司实习
+                    实习、疫情、备战秋招，最终拿到几个offer：科大讯飞、大华股份、华为等等；最终选择去华为。
                 </p>
                 <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    疫情突然来袭，不过这时候管控还不是很严，过完年，我们还是正常上班，主管希望我转正留下来，但我还是想去更大的平台。
-                </p>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    然后我提了离职，专心准备秋招。第一份简历是6月份开始投递，当时的我对大厂好像没什么概念，只是不停投简历、笔试、面试、复盘。我也没有什么目标，倒觉得这个过程还蛮有意思。
-                </p>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    也拿到几个offer：科大讯飞、大华股份、华为。
+                    2021年，顺利毕业。
                 </p>
                 <div className="grid grid-cols-2 gap-4">
                     <Image
@@ -77,19 +65,6 @@ const data = [
     },
     {
         title: "2022.10",
-        content: (
-            <div>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    我在交易中台工作了差不多1年的时间，包括试用期在内。之后接手一个重构项目，我很喜欢这个项目，它很有价值并且对于工作不久的我来说，很有挑战。
-                </p>
-                <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
-                    🎯 这个项目叫做BCP，他是一个专门针对交易系统的一个旁路校验服务；通过交易链路中产生的日志，关联核对关键信息，以此保证每次交易链路的资金一致性；用户可以在BCP平台上将业务中的日志进行关联，配置特定条件下触发特定的事件。
-                </p>
-            </div>
-        ),
-    },
-    {
-        title: "2023.06",
         content: (
             <div>
                 <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-base font-normal mb-4">
