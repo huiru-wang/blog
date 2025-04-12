647bf326457485c5c73652dc465525937f50669d
huiru-wang <huiru-wang@outlook.com>
2025-04-13 03:00:14 +0800

    ui

--- src/components/markdown/BlockQuote.tsx
+++ src/components/markdown/BlockQuote.tsx
@@ -3,8 +3,8 @@
 export default function BlockQuote({ children }) {
 
     return (
-        <div className="my-2 border-l-4 border-[#8184ba] bg-[#F3F0FF] bg-opacity-70 rounded-e shadow-[1px_1px_2px_0_var(--border)]">
-            <div className="text-gray-700 px-3">{children}</div>
+        <div className="my-2 border-l-4 border-[#d54a4a] bg-[#F3F0FF] bg-opacity-60 rounded-e shadow-[1px_1px_2px_0_var(--border)]">
+            <div className="text-black px-3">{children}</div>
         </div>
     );
 }
