54e7b6a8606e1fd11d92ac797f8c207d6849928c
huiru-wang <huiru-wang@outlook.com>
2025-09-06 16:45:21 +0800

    build opt

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false,
+  experimental: {
+    serverSourceMaps: false, // 禁用实验性的服务器源映射
+  },
   images: {
     remotePatterns: [
       {
