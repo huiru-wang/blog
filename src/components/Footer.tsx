d1aaf2e326a9a0fdf17f484d976bb6b4a2960f8a
huiru-wang <huiru-wang@outlook.com>
2025-09-17 00:37:29 +0800

    deploy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,9 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="mt-16">
+            <div className="text-sm text-gray-500 my-10">
+                <p>Copyright © 2025 Robin Wang. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
